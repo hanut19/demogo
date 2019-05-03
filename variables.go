b02dbbb6f7e13b8c852f66967a2f3c4ae7598a0e
amandeep <amandeep@webonise.com>
2019-05-03 18:01:38 +0530

    variables

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int = 1, 2, 3
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var d bool = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := 5
+	fmt.Println("The value of f is ", f)
+
+}
